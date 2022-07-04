0511f865fbf2b1e93ad76a4e911ca6798a20bd00
theonetruepatrick <104328646+theonetruepatrick@users.noreply.github.com>
2022-07-03 23:11:03 -0400

    Update Tank_Temp-and-Level.ino

--- Tank_Temp-and-Level.ino
+++ Tank_Temp-and-Level.ino
@@ -1,12 +1,14 @@
 /*
  * Board: ESP8266
  *    Temp Meter: DS18B20
+ *    Depth Meter: JSN-SR04T ultrasonic distance sensor
  *    
  *    
  *Config:
- *   GPIO2 => D4 => Data (DS18B20
+ *    GPIO2 => D4 => Data (DS18B20)
+ *    GPIO0 => D3 => Echo TX (JSN-SR04T)
+ *    GPIO4 => D2 => Trig DX (JSN-SR04T)
  * 
- * level sensing 2 branch
  * 
  * 
  * 
@@ -16,17 +18,62 @@
 //Core Coding
   #include "core_iot.h"
 
-
+// Pin Connections
+#define TempSensor 5  //GPIO5 => D1 => Data (DS18B20)
+#define trigPin 0        //GPIO4 => D3 => Trig DX (JSN-SR04T)
+#define echoPin 2        //GPIO2 => D4 => Trig DX (JSN-SR04T)
 
 //Temperature Reading Config
   #include <OneWire.h>
   #include <DallasTemperature.h>
-  #define TempSensor_01 2  //Temp Sensor DATA 
-  OneWire oneWire(TempSensor_01);
+  OneWire oneWire(TempSensor);
   DallasTemperature sensors(&oneWire); 
 
-//Declare Variables
-  //extern float TempF = 0;  //Temperature
+  // Define variables for sensor:
+    float duration;               //Sensor pulse
+    float distance;               //Sensor pulse
+    float tankLevel;              //the % of the tank level
+  
+  //Variables to set specific to each tank
+    float tankFull = 10;          //minimum distance (inches) between sensor and water level aka FULL
+    float tankEmpty = 21;         //maximum distance (inches) between sensor and water level aka EMPTY
+    float tankLevelMax = 100;     //highest value (%) allowed for tank level
+    float tankLevelMin = 0;       //lowest value (%) allowed for tank level
+    float distanceOffset = 1.0;   //compensates for reading in INCHES - not sure if this is common across all sensors
+    float tankAlarm = 30;         //Threshold for alerting if level falls below this %
+    
+    const float speedOfSound = 0.0135;  //speed of sound: inches per MICROsecond
+    float durationTimeout = (tankEmpty*1.5*2)/speedOfSound;  //used to shorten wait time for pulse 
+  
+  // Define variables for Data Analysis
+    //sampling factors
+      int readingDelayMS = 60000;  //time gap between readings; default is 60 seconds
+      int readingDelayAdjustment;  //me being WAY too anal retentive
+      const int numReadings = 15;  //number of readings to be averaged/smoothed
+      int readingCount=0;          //current total number of readings
+      
+    //Data Smoothing
+      unsigned long readingTimestamp = 0;    //timestamp [milliseconds] variable for reading cycle
+      int readings[numReadings];   //array that stores readings
+      int readIndex = 0;           //index for readings variable
+      float readingSumTotal = 0;
+      float readingAverage = 0;
+
+    //Rate of Change calcuation
+      float readingDelta = 0;
+      int ROCIndex = 0;           // "ROC" = "Rate of Change"
+      float ROCSumTotal = 0;
+      float readingROC[numReadings];  //array that stores deltas of the running averages
+      float ROCTrend = 0;
+
+    //Time To Empty
+      float hrsToEmpty=0;
+      String lowSampleFlag;
+
+
+
+
+
  
 void setup(void)
 {
@@ -44,6 +91,10 @@ void setup(void)
   
   // Start up the Temperature Sensor library
   sensors.begin();
+
+   // Define inputs and outputs
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
  
  
